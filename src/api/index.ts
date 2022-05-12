ca0845071e3450aefa327f23b9aab83a1fe66c39
박하민 <haminjp@naver.com>
2022-05-12 20:34:04 +0900

    [수정] axios fq 예시 주석으로 기록

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,6 +5,14 @@ export const axiosGetArticles = async () => {
     .get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {
       params: {
         "api-key": "9HjHkzg0V8sD7Eg9sIdTUu9stiqsL5RU",
+        // fq: 'section_name:("World") AND glocations:("South Korea")',
+        // fq: 'section_name:("World") AND glocations:("China")',
+        // fq: 'section_name:("World") AND glocations:("Japan")',
+        // fq: 'section_name:("World") AND glocations:("United States")',
+        // fq: 'section_name:("World") AND glocations:("North Korea")',
+        // fq: 'section_name:("World") AND glocations:("Russia")',
+        // fq: 'section_name:("World") AND glocations:("France")',
+        // fq: 'section_name:("World") AND glocations:("England") AND glocations:("France")',
       },
     })
     .then((response) => response.data)
