3aaac8f0b1f34a94f1a25cf112ae746c2339f779
박하민 <haminjp@naver.com>
2022-05-14 22:01:55 +0900

    [수정] Scrap 필터 바와 모달 구현

--- src/components/ScrapFilterBar.tsx
+++ src/components/ScrapFilterBar.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
+import FilterButton from "./FilterButton";
 import SearchIcon from "../img/searchIcon.png";
+import SearchIconBlue from "../img/searchIconBlue.png";
 import CalendarIcon from "../img/calendarIcon.png";
+import CalendarIconBlue from "../img/calendarIconBlue.png";
+import ScrapFilterModal from "./ScrapFilterModal";
 import { useSelector } from "react-redux";
 import { RootState } from "../modules";
-import ScrapFilterModal from "./ScrapFilterModal";
-import FilterButton from "./FilterButton";
 
 const Container = styled.div`
   width: 100%;
@@ -21,13 +23,31 @@ const ScrapFilterBar = () => {
   const isScrapModalOpen = useSelector(
     (state: RootState) => state.news.scrapFilter.modalOpen
   );
+  const scrapFilter = useSelector((state: RootState) => state.news.scrapFilter);
+
+  const getCountryLabel = () => {
+    if (scrapFilter.country.length === 0) return "전체 국가";
+    else
+      return `${scrapFilter.country[0]} 외 ${scrapFilter.country.length - 1}개`;
+  };
 
   return (
     <>
       <Container>
-        {/* <FilterButton iconSrc={SearchIcon} label={"전체 헤드라인"} />
-        <FilterButton iconSrc={CalendarIcon} label={"전체 날짜"} />
-        <FilterButton label={"전체 국가"} /> */}
+        <FilterButton
+          iconSrc={scrapFilter.headline === "" ? SearchIcon : SearchIconBlue}
+          label={scrapFilter.headline || "전체 헤드라인"}
+          isSelected={scrapFilter.headline !== ""}
+        />
+        <FilterButton
+          iconSrc={scrapFilter.date === "" ? CalendarIcon : CalendarIconBlue}
+          label={scrapFilter.date || "전체 날짜"}
+          isSelected={scrapFilter.date !== ""}
+        />
+        <FilterButton
+          label={getCountryLabel()}
+          isSelected={scrapFilter.country.length !== 0}
+        />
       </Container>
       {isScrapModalOpen && <ScrapFilterModal />}
     </>
--- src/components/ScrapFilterModal.tsx
+++ src/components/ScrapFilterModal.tsx
@@ -1,4 +1,25 @@
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import useActionCreators from "../hooks/useActionCreators";
+import CalendarGray from "../img/calendarIcon.png";
+import { Filter } from "../interfaces";
+import { RootState } from "../modules";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { ko } from "date-fns/esm/locale";
+import { formatDate } from "../utils";
+
+const COUNTRIES = [
+  "대한민국",
+  "중국",
+  "일본",
+  "미국",
+  "북한",
+  "러시아",
+  "프랑스",
+  "영국",
+];
 
 const Background = styled.div`
   background-color: black;
@@ -21,6 +42,13 @@ const Container = styled.div`
   width: calc(100% - 40px);
   height: 480px;
   padding: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Inputs = styled.div`
+  width: 100%;
+  flex: 1;
 `;
 
 const Label = styled.div`
@@ -28,6 +56,52 @@ const Label = styled.div`
   line-height: 24px;
   font-weight: 600;
   margin-bottom: 7px;
+`;
+
+const HeadLineInput = styled.input`
+  width: 100%;
+  height: 44px;
+  border: 1px solid #c4c4c4;
+  border-radius: 8px;
+  padding: 10px 20px;
+  font-size: 14px;
+  margin-bottom: 40px;
+
+  &::placeholder {
+    color: #c4c4c4;
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+const DateInput = styled.div`
+  width: 100%;
+  height: 44px;
+  border: 1px solid #c4c4c4;
+  border-radius: 8px;
+  padding: 10px 20px;
+  font-size: 14px;
+  margin-bottom: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Countries = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+`;
+
+const CountryButton = styled.div<{ isSelected: boolean }>`
+  height: 34px;
+  padding: 6px 12px 4px;
+  border-radius: 30px;
+  border: 1px solid #f2f2f2;
+  font-size: 14px;
+  color: ${({ isSelected }) => (isSelected ? "white" : "#6d6d6d")};
+  background-color: ${({ isSelected }) => (isSelected ? "#82B0F4" : "white")};
 `;
 
 const ApplyButton = styled.button`
@@ -44,13 +118,121 @@ const ApplyButton = styled.button`
 `;
 
 const ScrapFilterModal = () => {
+  const headlineInputRef = useRef<HTMLInputElement>(null);
+
+  const [filterValues, setFilterValues] = useState<Filter>();
+
+  const scrapFilter = useSelector((state: RootState) => state.news.scrapFilter);
+
+  const { closeScrapModal, setScrapFilter } = useActionCreators();
+
+  useEffect(() => {
+    setFilterValues(scrapFilter);
+
+    if (headlineInputRef.current === null) return;
+    headlineInputRef.current.value = scrapFilter.headline;
+  }, [scrapFilter]);
+
+  const onHeadLineInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (filterValues === undefined) return;
+
+    const newHeadLine = e.target.value;
+    const newFilterValues: Filter = {
+      ...filterValues,
+      headline: newHeadLine,
+    };
+
+    setFilterValues(newFilterValues);
+  };
+
+  const onDateChange = (date: Date) => {
+    if (filterValues === undefined) return;
+
+    const newDate = formatDate(date);
+    const newFilterValues: Filter = {
+      ...filterValues,
+      date: newDate,
+    };
+
+    setFilterValues(newFilterValues);
+  };
+
+  const onCountryButtonClick = (country: string) => {
+    let newCountry;
+    if (filterValues === undefined) {
+      newCountry = [country];
+    } else if (filterValues.country.includes(country)) {
+      newCountry = [...filterValues.country.filter((c) => c !== country)];
+    } else {
+      newCountry = [...filterValues.country, country];
+    }
+
+    const newFilterValues: Filter = {
+      ...scrapFilter,
+      country: newCountry,
+    };
+
+    setFilterValues(newFilterValues);
+  };
+
+  const datePickerCustomInput = () => {
+    return (
+      <div>
+        {filterValues?.date ? (
+          filterValues?.date
+        ) : (
+          <span style={{ color: "#c4c4c4" }}>{"날짜를 정해주세요."}</span>
+        )}
+      </div>
+    );
+  };
+
+  const onApplyButtonClick = () => {
+    if (filterValues !== undefined) setScrapFilter(filterValues);
+    closeScrapModal();
+  };
+
   return (
     <>
       <Container>
-        <Label>헤드라인</Label>
-        <Label>날짜</Label>
-        <Label>국가</Label>
-        <ApplyButton>필터 적용하기</ApplyButton>
+        <Inputs>
+          <Label>헤드라인</Label>
+          <HeadLineInput
+            ref={headlineInputRef}
+            placeholder={"검색할 헤드라인을 입력해주세요."}
+            onChange={onHeadLineInputChange}
+          />
+
+          <Label>날짜</Label>
+          <DateInput>
+            <DatePicker
+              onChange={onDateChange}
+              locale={ko}
+              dateFormat="yyyy.MM.dd"
+              maxDate={new Date()}
+              customInput={datePickerCustomInput()}
+              popperPlacement="bottom-end"
+              showPopperArrow={false}
+            />
+            <img src={CalendarGray} alt="date" />
+          </DateInput>
+
+          <Label>국가</Label>
+          <Countries>
+            {COUNTRIES.map((country) => (
+              <CountryButton
+                key={country}
+                onClick={() => onCountryButtonClick(country)}
+                isSelected={
+                  filterValues?.country.includes(country) ? true : false
+                }
+              >
+                {country}
+              </CountryButton>
+            ))}
+          </Countries>
+        </Inputs>
+        <ApplyButton onClick={onApplyButtonClick}>필터 적용하기</ApplyButton>
       </Container>
       <Background />
     </>
